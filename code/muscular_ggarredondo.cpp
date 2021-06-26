3f62857fac78dc592a638e145ce08e70857bd7cc
ggarredondo <ggarredondo@correo.ugr.es>
2021-06-26 22:33:25 +0200

    backup commit

--- code/muscular_ggarredondo.cpp
+++ code/muscular_ggarredondo.cpp
@@ -12,27 +12,29 @@ void printv(std::vector<T> v) {
 }
 
 // Implementación del algoritmo muscular
-std::vector<double> muscular(const std::vector<double>& ini, size_t max_evals, size_t seed)
+std::vector<double> muscular(const std::vector<double>& ini, size_t max_fails, size_t max_evals, size_t seed)
 {
     // obtención de la distribución de músculos
     std::vector<size_t> distribucion; // distribución de los músculos (indica dónde comienza cada músculo)
     distribucion.push_back(0); // el primer músculo siempre empieza en el 0
-    size_t actual = 0, size = ini.size();
-    std::uniform_int_distribution<size_t> muscle_dis(1, size*0.5); // un músculo será por lo menos de tamaño 1 y como máximo
+    size_t actual = 0, dim = ini.size();
+    std::uniform_int_distribution<size_t> muscle_dis(1, dim*0.5); // un músculo será por lo menos de tamaño 1 y como máximo
     std::mt19937 gen(seed);                                              // la mitad del vector solución
 
     actual += muscle_dis(gen);
-    while (actual < size) {
+    while (actual < dim) {
         distribucion.push_back(actual);
         actual += muscle_dis(gen);
     }
 
     // minimización del fitness
-    std::vector<double> sol = ini;
+    std::vector<double> sol = ini, aux;
     std::uniform_real_distribution<double> dis(-100,100); // rango de valores del vector solución [-100, 100]
     for (size_t ev = 0; ev < max_evals;) {
         size_t index = rand()%distribucion.size(); // escoger un músculo aleatorio a entrenar
-        
+        for (size_t i = distribucion[index], fails = 0; ev < max_evals && fails < max_fails; i = (i+1)%distribucion[index]) {
+            
+        }
     }
 
     return sol;
